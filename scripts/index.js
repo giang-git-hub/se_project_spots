2be07d476fc92b3eb1372f5ad93532965588c3c1
giang-git-hub <phong.giang915@gmail.com>
2025-10-23 02:08:40 -0400

    Add files via upload

--- scripts/index.js
+++ scripts/index.js
@@ -44,30 +44,38 @@ const profileDescriptionEl = document.querySelector(".profile__description");
 const cardImageEl = document.querySelector(".card__image");
 const cardCaptionEL = document.querySelector(".card__title");
 
+function openModal(modal) {
+  modal.classlist.add("modal_is-opened");
+}
+
+function closeModal(modal) {
+  modal.classlist.remove("modal_is-opened");
+}
+
 editProfileBtn.addEventListener('click', function() {
   editProfileNameInput.value = profileNameEl.textContent;
   editProfileDescriptionInput.value = profileDescriptionEl.textContent;
-  editProfileModal.classList.add("modal_is-opened");
+  openModal(editProfileModal);
 });
 
 editProfileCloseBtn.addEventListener('click', function() {
-  editProfileModal.classList.remove("modal_is-opened");
+  closeModal(editProfileModal);
 });
 
 
 newPostBtn.addEventListener('click', function() {
-  newPostModal.classList.add("modal_is-opened");
+  openModal(newPostModal);
 });
 
 newPostCloseBtn.addEventListener('click', function() {
-  newPostModal.classList.remove("modal_is-opened");
+  closeModal(newPostModal);
 });
 
 function handleEditProfileFormSubmit(evt) {
   evt.preventDefault();
   profileNameEl.textContent = editProfileNameInput.value;
   profileDescriptionEl.textContent = editProfileDescriptionInput.value;
-  editProfileModal.classList.remove("modal_is-opened");
+  closeModal(editProfileModal);
 }
 
 editProfileForm.addEventListener("submit", handleEditProfileFormSubmit);
